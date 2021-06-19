30efb3b22d95930652d0eb66fd03a5b663b3065c
Sean Mahoney <sean@dtprosusa.com>
2021-06-19 13:39:49 -0400

    Update content.ts

--- src/containers/about/content.ts
+++ src/containers/about/content.ts
@@ -17,6 +17,7 @@ const content = {
       newTab: true,
     },
   ],
+  summary: 'Experienced Web Developer adept in all stages of advanced web development. Knowledgeable in user interface, testing, and debugging processes. Bringing forth expertise in design, installation, testing and maintenance of web systems.',
   jobTitle1: 'Front-End Developer (AEM)',
   jobCompany1: 'DT Professional Services LLC',
   jobLocation1: 'New York, NY',
