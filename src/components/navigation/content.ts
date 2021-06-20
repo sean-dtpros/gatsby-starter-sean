91cedbb88b983fb1de2258531f2430b7c4489576
Sean Mahoney <sean@dtprosusa.com>
2021-06-20 13:11:27 -0400

    Update content.ts

--- src/components/navigation/content.ts
+++ src/components/navigation/content.ts
@@ -2,6 +2,7 @@ const options = [
   { to: '/', text: 'Home' },
   { to: '/blog/', text: 'Blog' },
   { to: '/about/', text: 'About' },
+  { to: '/resume/', text: 'Resume' },
 ];
 
 export default options;
