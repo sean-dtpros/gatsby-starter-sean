3f16aec03d1e38d05c97372ab9ae5d62aba4499a
Sean Mahoney <sean@dtprosusa.com>
2021-06-21 19:53:58 -0400

    Update JobPost.tsx

--- src/components/job-post/JobPost.tsx
+++ src/components/job-post/JobPost.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { withStyles } from '@material-ui/styles';
 import Typography from '@material-ui/core/Typography';
+import { Grid } from '@material-ui/core';
 
 import styles from './style';
 
@@ -11,6 +12,27 @@ interface Props {
 
 const JobPost = ({ classes, data }: Props) => (
   <div className={classes.container}>
+      <Grid item xs={6}>
+    <Item><Typography variant='body2' color='textPrimary'>
+      {data.position}
+    </Typography>
+    <Typography variant='body2' color='textPrimary'>
+      {data.company}
+    </Typography></Item>
+  </Grid>
+    <Grid item xs={6}>
+    <Item>    <Typography variant='body1' color='textPrimary'>
+      {data.startDate}
+    </Typography>
+    <Typography variant='body1' color='textPrimary'>
+      {data.endDate}
+    </Typography>
+    <Typography variant='body1' color='textPrimary'>
+      {data.location}
+    </Typography></Item>
+  </Grid>
+
+</Grid>
     <Typography variant='body2' color='textPrimary'>
       {data.position}
     </Typography>
