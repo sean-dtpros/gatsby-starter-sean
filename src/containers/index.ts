83dd5fa61546c4ece969c9f457129f0f45bd9b24
Sean Mahoney <sean@dtprosusa.com>
2021-06-20 22:46:38 -0400

    2

--- src/containers/index.ts
+++ src/containers/index.ts
@@ -2,4 +2,3 @@ export { default as About } from './about';
 export { default as BlogPostList } from './blog-post-list';
 export { default as Welcome } from './welcome';
 export { default as JobPostList } from './job-post-list';
-export { default as Timeline } from './timeline';
--- src/pages/resume.tsx
+++ src/pages/resume.tsx
@@ -1,14 +1,12 @@
 import React from 'react';
 
 import { Layout, SEO } from 'components';
-import { Timeline } from 'containers';
 import { JobPostList } from 'containers';
 
 const ResumePage = () => (
   <Layout noBackground>
     <SEO title='Resume' />
     <JobPostList />
-    <Timeline />
   </Layout>
 );
 
