798303723b9661552488c4dd1d2d8680fb55a3aa
Sean Mahoney <sean@dtprosusa.com>
2021-06-20 22:14:56 -0400

    Update resume.tsx

--- src/pages/resume.tsx
+++ src/pages/resume.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 
 import { Layout, SEO } from 'components';
+import { Timeline } from 'containers';
 import { JobPostList } from 'containers';
 
 const ResumePage = () => (
   <Layout noBackground>
     <SEO title='Resume' />
     <JobPostList />
+    <Timeline />
   </Layout>
 );
 
