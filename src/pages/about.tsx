5fb4843fb919cc44eb0b8b0400d14425b6d4f0bd
Sean Mahoney <sean@dtprosusa.com>
2021-06-21 21:45:56 -0400

    Update about.tsx

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -2,11 +2,13 @@ import React from 'react';
 
 import { Layout, SEO } from 'components';
 import { About } from 'containers';
+import { JobPostList } from 'containers';
 
 const AboutPage = () => (
   <Layout>
     <SEO title='About' />
     <About />
+    <JobPostList />
   </Layout>
 );
 
