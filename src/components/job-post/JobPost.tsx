dc3f5ae748890dc71acefce8df8b978ff5bd3212
Sean Mahoney <sean@dtprosusa.com>
2021-06-21 21:59:07 -0400

    Update JobPost.tsx

--- src/components/job-post/JobPost.tsx
+++ src/components/job-post/JobPost.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import { withStyles } from '@material-ui/styles';
 import Typography from '@material-ui/core/Typography';
+import Grid from '@material-ui/core/Grid';
+import Box from '@material-ui/core/Box';
 
 import styles from './style';
 
