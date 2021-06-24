b25ecffcee2cfe56297190cefe93e50b5990e60a
Sean Mahoney <sean@dtprosusa.com>
2021-06-23 21:36:15 -0400

    Revert "10"
    This reverts commit fdc6b157d21a3438fa114c151d94d44b1c075be9.

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -2,13 +2,11 @@ import React from 'react';
 
 import { Layout, SEO } from 'components';
 import { About } from 'containers';
-import { JobPostList } from 'containers';
 
 const AboutPage = () => (
   <Layout>
     <SEO title='About' />
     <About />
-    <JobPostList />
   </Layout>
 );
 
