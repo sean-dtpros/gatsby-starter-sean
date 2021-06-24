39cc8f191bc7b302d7f6379ecfce680c00d0261f
Sean Mahoney <sean@dtprosusa.com>
2021-06-23 21:37:37 -0400

    Revert "resumepage"
    This reverts commit 6f2185693e506fa6e93e2a22d6cb569fa9035dfc.

--- src/components/navigation/content.ts
+++ src/components/navigation/content.ts
@@ -2,7 +2,6 @@ const options = [
   { to: '/', text: 'Home' },
   { to: '/blog/', text: 'Blog' },
   { to: '/about/', text: 'About' },
-  { to: '/resume/', text: 'Resume' },
 ];
 
 export default options;
