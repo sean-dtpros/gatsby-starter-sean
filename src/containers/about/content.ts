00fc19b87480e29875124d5457f9585a5d7487a8
Sean Mahoney <sean@dtprosusa.com>
2021-06-23 21:37:26 -0400

    Revert "more cleanup"
    This reverts commit f82b301d5bd9bb9b47c786e56c204f9ef1dfc204.

--- src/containers/about/content.ts
+++ src/containers/about/content.ts
@@ -18,6 +18,12 @@ const content = {
     },
   ],
   summary: 'Experienced Web Developer adept in all stages of advanced web development. Knowledgeable in user interface, testing, and debugging processes. Bringing forth expertise in design, installation, testing and maintenance of web systems.',
+  jobTitle1: 'Front-End Developer (AEM)',
+  jobCompany1: 'DT Professional Services LLC',
+  jobLocation1: 'New York, NY',
+  jobDate1: 'October 2020 - Present',
+
+
 };
 
 export default content;
