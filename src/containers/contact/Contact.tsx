bf71ecb2103172430b66085539b67b9feeebcd88
Sean Mahoney <sean@dtprosusa.com>
2021-06-24 21:29:26 -0400

    Update Contact.tsx

--- src/containers/contact/Contact.tsx
+++ src/containers/contact/Contact.tsx
@@ -1,13 +1,21 @@
 import React from 'react';
-import { Box } from '@material-ui/core';
-import ContactForm from 'components';
+import { useForm } from 'react-hook-form';
+
+const { register, handleSubmit, formState: { errors } } = useForm();
+const onSubmit = data => console.log(data);
+console.log(errors);
 
 const Contact = () => (
   <>
-    <Box>
-    <ContactForm />
-    </Box>
-  </>
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <input type="text" placeholder="First name" {...register("First name", {required: true, maxLength: 80})} />
+      <input type="text" placeholder="Last name" {...register("Last name", {required: true, maxLength: 100})} />
+      <input type="text" placeholder="Email" {...register("Email", {required: true, pattern: /^\S+@\S+$/i})} />
+      <textarea {...register("Message", {required: true, maxLength: 500})} />
+
+      <input type="submit" />
+    </form>
+    </>
 );
 
 export default Contact;
