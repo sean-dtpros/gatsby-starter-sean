e9867f6f3090be75102c070ed8522544f948a860
Sean Mahoney <sean@dtprosusa.com>
2021-06-24 21:16:21 -0400

    Revert "Update content.ts"
    This reverts commit 84d984e81115c2a218c9b3f6ac02e7fd998fd63e.

--- src/components/navigation/content.ts
+++ src/components/navigation/content.ts
@@ -2,8 +2,6 @@ const options = [
   { to: '/', text: 'Home' },
   { to: '/blog/', text: 'Blog' },
   { to: '/about/', text: 'About' },
-  { to: '/contact/', text: 'Contact' },
-
 ];
 
 export default options;
