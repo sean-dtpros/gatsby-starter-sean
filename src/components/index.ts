d29580276bb25d87c591cbdd06997db4444288bd
Sean Mahoney <sean@dtprosusa.com>
2021-06-24 21:42:26 -0400

    11

--- src/components/index.ts
+++ src/components/index.ts
@@ -9,4 +9,3 @@ export { default as Modal } from './modal';
 export { default as Navigation } from './navigation';
 export { default as SEO } from './seo';
 export { default as JobPost } from './job-post';
-export { default as ContactForm } from './contact-form';
