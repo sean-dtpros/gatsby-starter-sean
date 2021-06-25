3ce9c98d3ad482588800429e5a8a4f11092fdfba
Sean Mahoney <sean@dtprosusa.com>
2021-06-24 21:06:22 -0400

    Update Contact.tsx

--- src/containers/contact/Contact.tsx
+++ src/containers/contact/Contact.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Box } from '@material-ui/core';
-import ContactForm from '../../components/contact-form/ContactForm';
+import ContactForm from 'components';
 
 const Contact = () => (
   <>
